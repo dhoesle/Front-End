857e7b818a4fc4adea2455177937f2bea79b1a68
dhoesle <dhoesle21@gmail.com>
2020-08-24 22:42:07 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AddToDo from './components/AddTodo'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AddToDo />
+    </div>
+  );
+}
+
+export default App;
